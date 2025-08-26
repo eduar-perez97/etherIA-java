25737e9b28554527b99225413cc9034de996da8b
Eduar-Perez <eduarp-741@hotmail.com>
2025-08-26 11:42:11 -0500

    Creación de test para instrucciones

---
+++ src/test/java/com/periferia/etheria/service/InstructionServiceTest.java
@@ -0,0 +1,157 @@
+package com.periferia.etheria.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.periferia.etheria.dto.InstructionDto;
+import com.periferia.etheria.entity.InstructionEntity;
+import com.periferia.etheria.exception.UserException;
+import com.periferia.etheria.repository.InstructionRepository;
+import com.periferia.etheria.security.JwtService;
+import com.periferia.etheria.util.Response;
+
+@ExtendWith(MockitoExtension.class)
+public class InstructionServiceTest {
+
+	@Mock
+	private InstructionRepository instructionRepository;
+	@Mock
+	private JwtService jwtService;
+
+	@InjectMocks
+	private InstructionService instructionService;
+	private InstructionDto instructionDto;
+	private String token;
+
+	@BeforeEach
+	void setUp() {
+		token = "fake-jwt-token";
+
+		instructionDto = new InstructionDto();
+		instructionDto.setId(1L);
+		instructionDto.setName("Instrucciones para el agente claude");
+		instructionDto.setInstruction("Te comportarás como un agente experto en finanzas");
+		instructionDto.setDescription("Evaluarás cada archivo financiero y darás una respuesta conciza y preciza");
+		instructionDto.setGeneral(false);
+		instructionDto.setIdUser("1088978987");
+		instructionDto.setAction(null);
+
+	}
+
+	@Test
+	void testDeleteInstructionSucces() {
+		instructionDto.setAction("delete");
+		when(jwtService.validateToken(token)).thenReturn(true);
+
+		Response<Object> response = instructionService.interactueInstruction(instructionDto, token);
+
+		assertEquals(response.getStatusCode(), 200);
+		assertTrue(response.getMessage().contentEquals("Instrucción eliminada con exito"));
+		assertEquals(true, response.getData());
+		verify(instructionRepository).deleteInstruction(instructionDto.getId(), token);
+
+	}
+
+	@Test
+	void testCreateInstructionSucces() {
+		instructionDto.setAction("create");
+		when(jwtService.validateToken(token)).thenReturn(true);
+		when(instructionRepository.getInstructionsGeneral(anyList())).thenAnswer(instructions -> null);
+
+		Response<Object> response = instructionService.interactueInstruction(instructionDto, token);
+
+		assertEquals(200, response.getStatusCode());
+		assertTrue(response.getMessage().contentEquals("Instrucción creada con exito"));
+		assertEquals(true, response.getStatusCode());
+		verify(instructionRepository).createInstruction(any(InstructionEntity.class));
+
+	}
+
+	@Test
+	void testCreateInstructionExist() {
+		instructionDto.setAction("create");
+		InstructionEntity instructiongeneral = new InstructionEntity();
+		instructiongeneral.setName("Instrucciones para el agente claude");
+
+		when(jwtService.validateToken(token)).thenReturn(true);
+
+		doAnswer(invocation -> {
+			List<InstructionEntity> list = invocation.getArgument(0);
+			list.add(instructiongeneral);
+			return null;
+		}).when(instructionRepository).getInstructionsGeneral(anyList());
+
+		UserException ex = assertThrows(UserException.class, () -> 
+		instructionService.interactueInstruction(instructionDto, token));
+
+		assertTrue(ex.getMessage().contains("Ya existe en las instucciones generales"));
+		verify(instructionRepository).getInstructionsGeneral(anyList());
+		verify(instructionRepository, never()).createInstruction(any());
+
+	}
+
+	@Test
+	void testUpdateistructionSucces() {
+		instructionDto.setAction("update");
+		when(jwtService.validateToken(token)).thenReturn(true);
+
+		Response<Object> response = instructionService.interactueInstruction(instructionDto, token);
+
+		assertEquals(200, response.getStatusCode());
+		assertTrue(response.getMessage().contentEquals("Instrucción modificada con exito"));
+		assertEquals(true, response.getData());
+		verify(instructionRepository).updateInstruction(any(InstructionEntity.class));
+
+	}
+
+	@Test
+	void testGetInstructionSucces() {
+		instructionDto.setAction("update");
+		when(jwtService.validateToken(token)).thenReturn(true);
+		when(instructionRepository.getInstructions(instructionDto.getIdUser())).thenReturn(Collections.emptyList());
+
+		Response<Object> response = instructionService.interactueInstruction(instructionDto, token);
+
+		assertEquals(200, response.getStatusCode());
+		assertTrue(response.getMessage().contentEquals("Instrucciónes consultadas con exito"));
+		assertEquals(true, response.getData());
+		verify(instructionRepository).getInstructions(instructionDto.getIdUser());
+
+	}
+
+	@Test
+	void testActionInvalidThrowException() {
+		instructionDto.setAction("otherAction");
+		when(jwtService.validateToken(token)).thenReturn(true);
+
+		assertThrows(UserException.class, () -> 
+		instructionService.interactueInstruction(instructionDto, token));
+
+	}
+
+	@Test
+	void testInvalidTokenThrowException() {
+		when(jwtService.validateToken(token)).thenReturn(false);
+
+		assertThrows(UserException.class, () -> 
+		instructionService.interactueInstruction(instructionDto, token));
+	}
+
+}
